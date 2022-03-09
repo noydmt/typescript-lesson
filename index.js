76ba2185c7f1095ffe4834b408639e923d8e3c1b
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-03-09 23:54:41 +0900

    関数の定義

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 var hello = 'hello';
 console.log(hello);
+function add(a, b) {
+    return a + b;
+}
+console.log(add(1, 2));
--- index.ts
+++ index.ts
@@ -1,2 +1,8 @@
 let hello:string = 'hello'
 console.log(hello)
+
+function add (a:number, b:number): number {
+  return a + b;
+}
+
+console.log(add(1,2));
