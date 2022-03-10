22341f554059332ce86a2a5151266b0ae1de70a7
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-03-10 22:54:00 +0900

    boolean, number, stringの変数定義と型判定

---
+++ typescript-type/index.js
@@ -0,0 +1,10 @@
+var hasValue = false;
+var count = 10;
+var float = 3.14;
+var negative = -0.12;
+var single = 'hello';
+console.log(typeof hasValue);
+console.log(typeof count);
+console.log(typeof float);
+console.log(typeof negative);
+console.log(typeof single);
---
+++ typescript-type/index.ts
@@ -0,0 +1,11 @@
+let hasValue: boolean = false
+let count: number = 10
+let float: number = 3.14
+let negative: number = -0.12
+let single: string = 'hello'
+
+console.log(typeof hasValue)
+console.log(typeof count)
+console.log(typeof float)
+console.log(typeof negative)
+console.log(typeof single)
